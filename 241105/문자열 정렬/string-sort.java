2df67dfc1ad4449a2afa8ef446bc568a039f4e43
inhaJoo <127509641+inhaJoo@users.noreply.github.com>
2024-11-05 21:28:47 +0900

    Title: 문자열 정렬, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241105/문자열 정렬/string-sort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String sortedStr = new String(chars);
+
+        System.out.println(sortedStr);
+
+    }
+}
