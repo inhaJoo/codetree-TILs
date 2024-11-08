87e6e718bbc544c28d99ad9b9889904c0cde0507
inhaJoo <127509641+inhaJoo@users.noreply.github.com>
2024-11-08 10:44:28 +0900

    Title: Date to Date, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241108/Date to Date/date-to-date.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        Scanner sc = new Scanner(System.in);
+
+        int m1, d1, m2, d2, result=0;
+
+        m1 = sc.nextInt();
+        d1 = sc.nextInt();
+        m2 = sc.nextInt();
+        d2 = sc.nextInt();
+
+        for(int i=m1; i<=m2; i++) {
+            if(i == m1)
+                result += num_of_days[i] - d1 + 1;
+            else if(i == m2)
+                result += d2;
+            else
+                result += num_of_days[i];
+        }
+
+       System.out.print(result);
+    }
+}
