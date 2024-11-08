0cc57f3661e0177143309f9fb41de1aefd7ce93a
inhaJoo <127509641+inhaJoo@users.noreply.github.com>
2024-11-08 15:16:43 +0900

    Title: 그 요일은, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241108/그 요일은/the-day-of-the-day.java
+++ 241108/그 요일은/the-day-of-the-day.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Main {
     static int calculator(int m1, int d1, int m2, int d2, String day) {
         int[] num_of_date = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] str_of_days = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        String[] days_arr = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int[] counts = new int[7];
 
         int startDay = 0;
         int endDay = 0;
         int diff;
+        int idx=0;
         int week;
-        int days;
+        int cnt=1;
+        int result=0;
 
         for(int i=1; i<m1; i++) 
             startDay += num_of_date[i];
@@ -22,21 +25,21 @@ public class Main {
         endDay += d2;
         diff = endDay - startDay;
         week = diff / 7;
-        days = diff % 7;
 
-        for(int i=0; i<str_of_days.length; i++) {
-            if(day.equals(str_of_days[i])) {
-                if(days == 0) {
-                    return week + 1;
-                }else{
-                    return 
-                }
+        for(int i=0; i<days_arr.length; i++) {
+            if(day.equals(days_arr[i])) {
+                idx=i;
             }
         }
 
+        for(int j=0; j<=week; j++) {
+            counts[idx] = cnt;
+            cnt++;
+        }
 
+        result = counts[idx];
 
-        return endDay - startDay;
+        return result;
     }
 
     public static void main(String[] args) {
@@ -52,9 +55,9 @@ public class Main {
         d2 = sc.nextInt();
         day = sc.next();
 
-        // calculator(m1, d1, m2, d2);
+        int result = calculator(m1, d1, m2, d2, day);
 
-        System.out.print(2 / 7);
+        System.out.print(result);
 
     }
 }
